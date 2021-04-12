0c0259936e063be41b0fa39ebabf482bb9e9358c
Alex Groß <alex@superschurke.de>
2021-04-12 12:15:04 +0200

    player-whisper

--- modules/player-whisper.js
+++ modules/player-whisper.js
@@ -1,16 +1,42 @@
-import {moduleName} from "../dsa5-meistertools.js";
-
-export function playerWhisper() {
+export async function playerWhisper() {
+    let optionsString = ''
+    game.users.forEach(user => {
+        optionsString += `<option value='${user._data.name}'>${user._data.name}</option>`
+    });
 
     new Dialog({
-        title: game.i18n.localize(`${moduleName}.playerWhisper`),
-        content: `<p>${game.i18n.localize(`${moduleName}.playerWhisperFeatures`)}</p>`,
+        title: 'Pssst... Leise!',
+        content: `
+      <form>
+        <div class="form-group">
+          <label>Empfänger</label>
+          <select name='recipient' id='whisperrecipient'>${optionsString}</select>
+        </div>
+        <div class="form-group">
+          <label>geheime Nachricht</label>
+          <input type='text' name='message'/>
+        </div>
+      </form>`,
         buttons: {
             yes: {
-                icon: '<i class="fas fa-check"></i>',
-                label: "...okay"
+                icon: "<i class='fas fa-check'></i>",
+                label: `Senden`,
+                callback: html => {
+                    let message = html.find('input[name=\'message\']');
+                    let recipient = html.find('select[name=\'recipient\']');
+                    if (message.val() !== '') {
+                        ChatMessage.create({
+                            user: game.user._id,
+                            content: 'Flüstert <i><b>"' + message.val() + '"</b></i>',
+                            whisper: ChatMessage.getWhisperRecipients(recipient.val())
+                        });
+                    }
+                }
             }
+        },
+        default: 'yes',
+        render: html => {
+            html.find('input[name=\'message\']').focus()
         }
     }).render(true);
-
 }

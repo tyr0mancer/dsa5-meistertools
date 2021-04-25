3fd1fe184600ecf8760ba9a42e62fa164a888944
Alex Groß <alex@superschurke.de>
2021-04-25 18:29:11 +0200

    random encounter

--- modules/random-encounter.js
+++ modules/random-encounter.js
@@ -1,11 +1,12 @@
 import {moduleName} from "../dsa5-meistertools.js";
 
 export async function randomEncounter() {
-
     const location = game.settings.get("dsa5-traveller", 'location')
+    const ausgabe = getRandomEncounter('nachwache', location)
+
     new Dialog({
         title: game.i18n.localize(`${moduleName}.randomEncounter`),
-        content: `<h2>Aktuelle Region</h2><pre>${JSON.stringify(location, null, 2)}</pre>`,
+        content: `<h2>Zufallsbegegnung</h2><pre>${ausgabe.text}</pre>`,
         buttons: {
             yes: {
                 icon: '<i class="fas fa-check"></i>',
@@ -13,4 +14,58 @@ export async function randomEncounter() {
             }
         }
     }).render(true).close();
+}
+
+
+function getRandomEncounter(type, location) {
+    console.clear()
+    const result = ALL_ENCOUNTER[type].filter(e => filterByLocation(e, location))
+    if (result)
+        return result[Math.floor(Math.random() * result.length)]
+    else return {}
+}
+
+
+function filterByLocation(entry, location) {
+   if (entry.biome)
+        if (!location.biome?.key || !entry.biome.split(',').includes(location.biome.key)) return false
+    if (entry.region) {
+        for (let regionKey in entry.region) {
+            let found = false
+            const currentLocation = location.region.find(r => r.key === regionKey).index.map(i => i.key)
+            const requestedLocations = entry.region[regionKey].split(',')
+            for (let curLec of currentLocation) {
+                console.log(curLec, requestedLocations)
+                if (requestedLocations.includes(curLec)) {
+                    found = true
+                    break
+                }
+            }
+            if (!found)
+                return false
+        }
+    }
+    return true
+}
+
+
+const ALL_ENCOUNTER = {
+    nachwache: [
+        {
+            biome: 'wald',
+            text: 'Ein Goblin schlägt sich so durch'
+        },
+        {
+            region: {politik: 'andergast'},
+            text: 'Ein Andergaster kommt zu Besuch'
+        },
+        {
+            region: {politik: 'albernia'},
+            text: 'Ein albernischer Bauer hat Bauchweh'
+        },
+        {
+            text: 'Kann immer mal passieren'
+        }
+
+    ]
 }

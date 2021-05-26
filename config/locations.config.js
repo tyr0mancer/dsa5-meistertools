40acc0eac2eaf22f2d0ce274fffbeb534a6c3fcb
Alex Groß <alex@superschurke.de>
2021-05-26 17:21:20 +0200

    UI improvements

--- config/locations.config.js
+++ config/locations.config.js
@@ -10,27 +10,65 @@ export default {
     ],
     biomes: [
         //https://www.pexels.com/de-de/foto/strasse-landschaft-natur-himmel-4406222/
-        {key: "village", name: "Dörfer und Weiler", img: "modules/dsa5-meistertools/assets/images/biomes/village.webp"},
+        {
+            key: "village", name: "Dörfer und Weiler",
+            img: "modules/dsa5-meistertools/assets/images/biomes/village.webp"
+        },
         //https://www.pexels.com/de-de/foto/grauer-beton-triumphbogen-umgeben-von-blumen-1055068/
-        {key: "city", name: "kleine und mittlere Städte", img: "modules/dsa5-meistertools/assets/images/biomes/city.webp"},
+        {
+            key: "city", name: "kleine und mittlere Städte",
+            img: "modules/dsa5-meistertools/assets/images/biomes/city.webp"
+        },
         //https://www.pexels.com/de-de/foto/foto-des-hauses-nahe-am-fluss-61381/
-        {key: "metropolis", name: "Große Städten und Metropolen", img: "modules/dsa5-meistertools/assets/images/biomes/metropolis.webp"},
+        {
+            key: "metropolis", name: "Große Städten und Metropolen",
+            img: "modules/dsa5-meistertools/assets/images/biomes/metropolis.webp"
+        },
         //https://www.pexels.com/de-de/foto/panorama-fotografie-des-grunen-feldes-440731/
-        {key: "plains", name: "weite Ebenen", img: "modules/dsa5-meistertools/assets/images/biomes/plains.webp"},
+        {
+            key: "plains", name: "weite Ebenen", herbmod: 0,
+            img: "modules/dsa5-meistertools/assets/images/biomes/plains.webp"
+        },
         //https://www.pexels.com/de-de/foto/grune-kiefern-1563604/
-        {key: "forest", name: "gemäßigte Wälder", img: "modules/dsa5-meistertools/assets/images/biomes/forest.webp"},
+        {
+            key: "forest", name: "gemäßigte Wälder", herbmod: 1,
+            img: "modules/dsa5-meistertools/assets/images/biomes/forest.webp"
+        },
+        //https://www.pexels.com/de-de/foto/baume-in-der-nahe-des-sees-unter-blauem-himmel-534040/
+        {
+            key: "marsh", name: "Sümpfe, Marschen und Moore", herbmod: -1,
+            img: "modules/dsa5-meistertools/assets/images/biomes/marsh.webp"
+        },
         //https://www.pexels.com/de-de/foto/landschaftsfotografie-des-berges-414122/
-        {key: "hills", name: "Hügel", img: "modules/dsa5-meistertools/assets/images/biomes/hills.webp"},
+        {
+            key: "hills", name: "Hügel", herbmod: 0,
+            img: "modules/dsa5-meistertools/assets/images/biomes/hills.webp"
+        },
         //https://www.pexels.com/de-de/foto/schneebedeckter-berg-1755243/
-        {key: "mountains", name: "Gebirge", img: "modules/dsa5-meistertools/assets/images/biomes/mountains.webp"},
+        {
+            key: "mountains", name: "Gebirge", herbmod: -1,
+            img: "modules/dsa5-meistertools/assets/images/biomes/mountains.webp"
+        },
         //https://www.pexels.com/de-de/foto/foto-der-bergkette-bedeckt-mit-schnee-unter-mond-908644/
-        {key: "arctic", name: "Ewiges Eis", img: "modules/dsa5-meistertools/assets/images/biomes/arctic.webp"},
+        {
+            key: "arctic", name: "Hoher Norden", herbmod: -4,
+            img: "modules/dsa5-meistertools/assets/images/biomes/arctic.webp"
+        },
         //https://www.pexels.com/de-de/foto/holz-licht-dammerung-landschaft-4239623/
-        {key: "rainforest", name: "Regenwald", img: "modules/dsa5-meistertools/assets/images/biomes/rainforest.webp"},
+        {
+            key: "rainforest", name: "Regenwald", herbmod: 0,
+            img: "modules/dsa5-meistertools/assets/images/biomes/rainforest.webp"
+        },
         //https://www.pexels.com/de-de/foto/grune-busche-in-der-wuste-998653/
-        {key: "desert", name: "Wüste", img: "modules/dsa5-meistertools/assets/images/biomes/desert.webp"},
+        {
+            key: "desert", name: "Wüste", herbmod: -4,
+            img: "modules/dsa5-meistertools/assets/images/biomes/desert.webp"
+        },
         //https://www.pexels.com/de-de/foto/landschaftsfoto-des-gewassers-1001682/
-        {key: "ocean", name: "Auf hoher See", img: "modules/dsa5-meistertools/assets/images/biomes/ocean.webp"},
+        {
+            key: "ocean", name: "Auf hoher See",
+            img: "modules/dsa5-meistertools/assets/images/biomes/ocean.webp"
+        },
     ],
     currentLocation: {
         locatorToken: null,

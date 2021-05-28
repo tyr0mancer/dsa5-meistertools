c20248b3030f755bf3cd351d418a0787dfed9afc
Alex Groß <alex@superschurke.de>
2021-05-28 18:19:16 +0200

    updated merchant gm view

--- config/merchants.config.js
+++ config/merchants.config.js
@@ -1,3 +1,22 @@
-export default {
+export const QUALITY = [
+    {key: 1, name: "Q1", description: "Absteigen und Spelunken"},
+    {key: 2, name: "Q2", description: "Tavernen"},
+    {key: 3, name: "Q3"},
+    {key: 4, name: "Q4"},
+    {key: 5, name: "Q5"}
+]
 
-}
+export const PRICE = [
+    {key: 1, name: "P1", sell: 0.75, buy: 0.10},
+    {key: 2, name: "P2", sell: 1, buy: 0.25},
+    {key: 3, name: "P3", sell: 1.25, buy: 0.5},
+    {key: 4, name: "P4", sell: 1.5, buy: 0.75},
+    {key: 5, name: "P5", sell: 2, buy: 1},
+]
+
+export const MERCHANT_TYPE = [
+    {key: "tavern", name: "Tavernen und Herbergen"},
+    {key: "merchant", name: "Händler"},
+]
+
+export const LIBRARY_ITEM_TYPES = ["meleeweapon", "armor", "equipment", "poison", "consumable", "rangeweapon"]
--- src/merchants.js
+++ src/merchants.js
@@ -1,7 +1,7 @@
 //import DSA5Payment from "../../../systems/dsa5/modules/system/payment.js";
 import ActorSheetdsa5NPC from "../../../systems/dsa5/modules/actor/npc-sheet.js";
 import {moduleName} from "../meistertools.js";
-
+import {MERCHANT_TYPE, PRICE, QUALITY, LIBRARY_ITEM_TYPES} from "../config/merchants.config.js";
 
 export default class MeistertoolsMerchantSheet extends ActorSheetdsa5NPC {
 
@@ -25,12 +25,53 @@ export default class MeistertoolsMerchantSheet extends ActorSheetdsa5NPC {
 
     async getData() {
         const data = super.getData();
-        mergeObject(data, {})
+        this.merchantFlags = this.actor.getFlag(moduleName, 'merchant')
+        mergeObject(data, {options: {QUALITY, PRICE, MERCHANT_TYPE, LIBRARY_ITEM_TYPES}, ...this.merchantFlags})
         return data;
     }
 
     activateListeners(html) {
         super.activateListeners(html);
+
+        html.find(".re-roll").click(event => {
+            const rollType = $(event.currentTarget).attr("data-roll-type")
+            console.log(rollType)
+        })
+
+        html.find(".show-settings").click(() => {
+            $(".edit-settings").toggle(100)
+            this.actor.setFlag(moduleName, "merchant.general.edit-settings", !this.merchantFlags?.general?.["edit-settings"] || true)
+        })
+
+        html.find(".add-category").click(() => {
+            const supply = this.merchantFlags?.supply || []
+            supply.push({})
+            this.actor.setFlag(moduleName, 'merchant.supply', supply)
+        })
+
+        html.find(".delete-category").click((event) => {
+            const categoryId = $(event.currentTarget).attr("data-category-id")
+            const {supply} = this.merchantFlags
+            supply.splice(categoryId, 1)
+            this.actor.setFlag(moduleName, 'merchant.supply', supply)
+        })
+
+        html.find("select").change(e => this._handleChange(e))
+        html.find("input").change(e => this._handleChange(e))
+    }
+
+    _handleChange(event) {
+        const categoryId = $(event.currentTarget).attr("data-category-id")
+        const name = event.currentTarget.name
+        const value = (event.currentTarget.type === "checkbox")
+            ? event.currentTarget.checked
+            : event.currentTarget.value
+        if (!categoryId)
+            return this.actor.setFlag(moduleName, "merchant." + name, value)
+
+        const {supply} = this.merchantFlags
+        supply[categoryId][name] = value
+        this.actor.setFlag(moduleName, 'merchant.supply', supply)
     }
 
 

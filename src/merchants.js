f2aacdc51d15878beffab3eb3a82887784ac5ebd
Alex Groß <alex@superschurke.de>
2021-06-05 21:50:18 +0200

    merchant view in player HUD

--- src/merchants.js
+++ src/merchants.js
@@ -77,6 +77,7 @@ export default class MeistertoolsMerchantSheet extends ActorSheetdsa5NPC {
 
     activateListeners(html) {
         super.activateListeners(html);
+        MeistertoolsUtil.addDefaultListeners(html);
 
         html.find(".re-roll").click(async event => {
             const rollType = $(event.currentTarget).attr("data-roll-type")
